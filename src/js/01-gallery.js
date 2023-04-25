f48cdf61fdc5f6b9a99f5d05e7b036d712321675
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-25 19:28:57 +0300

    added simplelightbox lib; js and css for gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      // const { preview, original, description } = item;
+      return `<li class="gallery__item">
+                <a class="gallery__link" href=${original}>
+                  <img class="gallery__image" src=${preview} alt=${description} />
+                </a>
+              </li>`;
+    })
+    .join('');
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery .gallery__link', {
+  overlayOpacity: 0.8,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
