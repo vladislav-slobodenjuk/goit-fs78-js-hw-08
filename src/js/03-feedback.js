aea337ab3033545fa88d957eea1cbf543b06b498
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-29 00:47:39 +0300

    refactor task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,75 +1,57 @@
 import throttle from 'lodash.throttle';
 const formEl = document.querySelector('.feedback-form');
-// const emailEl = document.querySelector("[name = 'email']");
-// const messageEl = document.querySelector("[name = 'message']");
 
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 let formState = {};
 
-const sevedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-
-if (sevedData) {
-  // Object.assign(formState, sevedData);
-  // console.log(formState);
-
-  console.log('there is saved state');
-  formState = sevedData;
-
-  // console.log('file: 03-feedback.js:21  formEl.elements:', formEl.elements);
-  // formEl.elements.forEach(elem => {
-  //   elem.value = formState[elem.name];
-  //   console.log('aa');
-  // });
-
-  const fields = Object.keys(formState);
-  console.log('file: 03-feedback.js:25  fields:', fields);
-
-  fields.forEach(field => {
-    formEl.elements[field].value = formState[field];
-  });
-
-  // const { email, message } = formEl.elements;
-  // email.value = sevedData[`${email.name}`];
-  // message.value = sevedData[`${message.name}`];
-} else {
-  console.log("there isn't saved state");
-}
-
-formEl.addEventListener('input', throttle(onImputChange, 500));
+window.addEventListener('DOMContentLoaded', onLoad);
+formEl.addEventListener('input', throttle(onInputChange, 500));
 formEl.addEventListener('submit', onSubmit);
 
-function onImputChange({ target }) {
-  // const formData = new FormData(formEl);
-  // const data = {};
-  // formData.forEach((value, field) => {
-  //   data[field] = value.trim();
-  // });
+function onLoad() {
+  const sevedData = getSavedData(LOCAL_STORAGE_KEY);
+  if (!sevedData) return console.log("there isn't saved data");
 
+  console.log('there is saved data:', sevedData);
+  formState = sevedData;
+
+  const fields = Object.keys(formState);
+  fields.forEach(field => (formEl.elements[field].value = formState[field]));
+}
+
+function getSavedData(storageKey) {
+  return JSON.parse(localStorage.getItem(storageKey));
+}
+
+function onInputChange({ target }) {
   formState[target.name] = target.value;
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formState));
-  console.log('state was updated', formState);
+  console.log('state was updated and saved:', formState);
 }
 
 function onSubmit(e) {
   e.preventDefault();
-  //
-  const formData = new FormData(formEl);
-  const data = {};
-  formData.forEach((value, field) => {
-    data[field] = value.trim();
-  });
-
+  const data = collectFormData(e.target);
   const values = Object.values(data);
 
   for (const item of values) {
     if (item.length === 0) return alert('fill in all fields');
   }
 
+  clearAllData(e.target);
   console.log('submited data:', data); // instead of return
+}
 
+function collectFormData(form) {
+  const formData = new FormData(form);
+  const data = {};
+  formData.forEach((value, field) => (data[field] = value.trim()));
+  return data;
+}
+
+function clearAllData(form) {
   localStorage.removeItem(LOCAL_STORAGE_KEY);
-  e.target.reset();
+  form.reset();
   formState = {};
-
-  console.log('state was cleaned');
+  console.log('state was cleared');
 }
